01e26e3feb0787b83587e526fabaaebf0826e5c2
THOTLA NARESH <thotlanaresh2002@gmail.com>
2024-12-19 23:30:12 +0530

    index.html

---
+++ index.js
@@ -0,0 +1,56 @@
+function a(){
+    console.log("h2 clicked")
+}
+function clickBtn(){
+    console.log("h2 clicked")
+}
+
+let imgElem=document.querySelector("img")
+function img(){
+    imgElem.style.borderRadius="50px"
+}
+let img1Elem=document.querySelector("img")
+function img(){
+    img1Elem.src="https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/348100/348106.jpg"
+}
+let ntrimg=document.getElementById("ntr")
+    ntrimg.ondblclick=function(){
+        ntrimg.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz6WMqMC5Gi_lmyOR5yPv-v_T_73Y9azYc8Q&s"
+    }
+let Btn1=document.getElementById("hiver")
+Btn1.onmouseover=function(){
+    Btn1.style.fontSize="50px"
+    Btn1.style.borderBlockColor="red"
+    Btn1.style.color="white"
+}
+let Btn2=document.getElementById("clickme")
+Btn2.onclick=function(){
+    Btn2.style.fontSize="50px"
+    Btn2.style.borderBlockColor="red"
+    Btn2.style.color="white"
+}
+let Btn3=document.getElementById("dblclickme")
+Btn3.ondblclick=function(){
+    Btn3.style.fontSize="50px"
+    Btn3.style.borderBlockColor="red"
+    Btn3.style.color="white"
+}
+let input=document.querySelector("input")
+input.onkeydown=function(){
+    document.body.style.backgroundColor="red"
+}
+
+function a(){
+    console.log("h2 clicked")
+}
+function clickBtn(){
+    alert("btn clicked")
+    console.log("h2 clicked")
+
+}
+// let imgElem=document.querySelector("img")
+// function img(){
+//     imgElem.style.borderRadius="50px"
+// }
+// imgElem.src="https://images.indianexpress.com/2024/10/Happy-Birthday-Prabhas.jpg"
+ 
